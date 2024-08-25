3b8ecbe4ff9f08dc25d42f4299a3cd608835e8ae
CaioAzvdo <caioazevedodourado8@gmail.com>
2024-08-24 21:01:18 -0300

    Funcao delete adicionada

--- src/app/_services/item.service.ts
+++ src/app/_services/item.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Observable} from "rxjs";
+import {UserAuthService} from "./user-auth.service";
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +15,15 @@ export class ItemService {
   });
   getItems(): Observable<any[]>{
     return this.http.get<any[]>(this.path_to_api + 'cd/listAll');
+  }
+  getItemById(id: number): Observable<any>{
+    return this.http.get<any>(this.path_to_api + 'cd/' + id);
+  }
+  deleteItemById(id: number, token: string): Observable<any>{
+    const headers = new HttpHeaders({
+      'Authorization': `${token}`
+    });
+      return this.http.delete<any>(this.path_to_api + 'cd/delete/' + id, { headers });
   }
 
 }
--- src/app/item-list/item-list.component.ts
+++ src/app/item-list/item-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ItemService} from "../_services/item.service";
+import {UserAuthService} from "../_services/user-auth.service";
 
 @Component({
   selector: 'app-item-list',
@@ -8,12 +9,18 @@ import {ItemService} from "../_services/item.service";
 })
 export class ItemListComponent implements OnInit {
 items: any[] = [];
-  constructor(private itemService : ItemService) { }
+  constructor(private itemService : ItemService,
+              private userAuthService: UserAuthService) { }
 
   ngOnInit(): void {
     this.itemService.getItems().subscribe((data) => {
       this.items = data;
       console.log(data);
+    });
+  }
+  deleteItem(id: number){
+    this.itemService.deleteItemById(id, this.userAuthService.getToken()).subscribe((data) => {
+      this.items = this.items.filter(item => item.id !== id);
     });
   }
 
